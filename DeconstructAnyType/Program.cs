eb45592b86a9a84266e8802e7fe955a995f3d238
zcomert <zafer.comert@hotmail.com>
2022-05-17 23:36:19 +0300

    Add project files.

---
+++ DeconstructAnyType/Program.cs
@@ -0,0 +1,17 @@
+﻿// Deconstruct 
+
+var (year, month, day) = DateTime.Now;
+
+Console.WriteLine($"Şu anki tarih : {day} {month} {year}.");
+Console.ReadKey();
+
+public static class DateTimeExtensions
+{
+    // !! Deconstruct
+    public static void Deconstruct(this DateTime datetime,
+        out int year,
+        out int month,
+        out int day) => (year, month, day) = (datetime.Year,
+        datetime.Month, datetime.Day);
+}
+
